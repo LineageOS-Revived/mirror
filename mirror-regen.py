e201e60f4dc8929a8c6fcc862f26ee173606e68d
SirRGB <sirrgb@proton.me>
2026-08-24 23:07:32 +0200

    adapt to revived

--- mirror-regen.py
+++ mirror-regen.py
@@ -17,7 +17,7 @@ except KeyError:
     print("Please set the GHUSER and GHTOKEN environment variables")
     exit(1)
 
-orgName = "LineageOS"
+orgName = "LineageOS-Revived"
 org = Github(u, p).get_user(orgName)
 
 file = open("default.xml", "w")
